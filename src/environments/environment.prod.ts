c5ef9a3b7350233c83772cf883a0dc0c8183d90b
Anuj Paul <anujpaul19@outlook.com>
2025-12-08 19:01:39 -0600

    added prod env file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    // baseUrl: 'https://anujresumeservice.azurewebsites.net'
+    //baseUrl: 'http://localhost:8085',
+    baseUrl: 'https://app.azure-api.net',
+    apimSubscriptionKey: 'ab80fa27aa0f459f999d885dc2bf33d9'
+
+};
