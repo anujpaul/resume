cb3753b5e0159ae68600391017006100e77e6db7
Anuj Paul <anujpaul19@outlook.com>
2025-12-11 21:07:46 -0600

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // baseUrl: 'https://app.azure-api.net',
-    baseUrl: 'http://localhost:8085',
-    apimSubscriptionKey: 'ab80fa27aa0f459f999d885dc2bf33d9'
+    baseUrl: 'https://resume.azure-api.net',
+    apimSubscriptionKey: 'bac3b53a10684a79977325d79356784f'
   };
   
