a65b56b65afcb632e0770cb1b21dd8c972015e8e
Anuj Paul <anujpaul19@outlook.com>
2025-12-11 21:01:42 -0600

    Consumption based APIM

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://app.azure-api.net',
+    // baseUrl: 'https://app.azure-api.net',
+    baseUrl: 'http://localhost:8085',
     apimSubscriptionKey: 'ab80fa27aa0f459f999d885dc2bf33d9'
   };
   
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     // baseUrl: 'https://anujresumeservice.azurewebsites.net'
     //baseUrl: 'http://localhost:8085',
-    baseUrl: 'https://app.azure-api.net',
-    apimSubscriptionKey: 'ab80fa27aa0f459f999d885dc2bf33d9'
+    baseUrl: 'https://resume.azure-api.net',
+    apimSubscriptionKey: 'bac3b53a10684a79977325d79356784f'
 
 };
